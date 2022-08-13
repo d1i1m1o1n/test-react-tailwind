04dad1eae3c4f3161ebd49eb1bc1a6e8816a9b79
d1i1m1o1n <d1i1m1o1n@gmail.com>
2022-08-13 11:58:31 +0300

    Fix styles and alt

--- src/components/WhatWillCourseGiveYou/WhatWillCourseGiveYou.jsx
+++ src/components/WhatWillCourseGiveYou/WhatWillCourseGiveYou.jsx
@@ -9,22 +9,22 @@ import nodejsIcon from "../../images/icons/nodejs.svg";
 
 const WhatWillCourseGiveYou = () => {
   return (
-    <div className="pt-32 pb-24 px-5 mb-20 bg-basic-black text-white rounded-[20px]">
-      <h2 className="text-[54px] font-semibold mb-20 text-center">Что тебе даст прохождение курса?</h2>
+    <div className="pt-32 pb-24 px-10 2xl:px-52 mb-20 bg-basic-black text-white rounded-[20px]">
+      <h2 className="text-[54px] font-semibold mb-10 text-center">Что тебе даст прохождение курса?</h2>
 
       <Tabs className="flex gap-10 justify-center text-2xl font-semibold mb-24">
         <Tab label="SKILLS UPGRADE">
           <div className="flex justify-around flex-wrap xl:flex-nowrap gap-12">
             <div className="flex flex-col items-center max-w-md gap-7 text-center">
-              <img src={codeIcon}/>
+              <img src={codeIcon} alt=""/>
               <p>Научишься понимать терминологию, документацию и писать код на уровне востребованных  разработчиков</p>
             </div>
             <div className="flex flex-col items-center max-w-md gap-7 text-center">
-              <img src={knowledgeIcon}/>
+              <img src={knowledgeIcon} alt=""/>
               <p>твои знания превратятся в систему, ты не просто изучишь технологии, но и поймешь как развиваться дальше</p>
             </div>
             <div className="flex flex-col items-center max-w-md gap-7 text-center">
-              <img src={fullstackIcon}/>
+              <img src={fullstackIcon} alt=""/>
               <p>Создашь Fullstack приложение, которое покажет что твои навыки выше чем у стандартного junior</p>
             </div>
           </div>
@@ -32,15 +32,15 @@ const WhatWillCourseGiveYou = () => {
         <Tab label="STRONG HARD SKILLS">
           <div className="flex justify-around flex-wrap xl:flex-nowrap gap-12">
             <div className="flex flex-col items-center max-w-md gap-7 text-center">
-              <img src={atomIcon}/>
+              <img src={atomIcon} alt=""/>
               <p>Создашь с помощью фреймворка React  визуальную часть приложения: элементы, анимация</p>
             </div>
             <div className="flex flex-col items-center max-w-md gap-7 text-center">
-              <img src={nodejsIcon}/>
+              <img src={nodejsIcon} alt=""/>
               <p>Изучишь NodeJS на достаточном уровне чтобы связывать frontend и backend</p>
             </div>
             <div className="flex flex-col items-center max-w-md gap-7 text-center">
-              <img src={knowledgeIcon}/>
+              <img src={knowledgeIcon} alt=""/>
               <p>Овладеешь базой,  от которого можно отталкиваться и учить любую технологию на JavaScript</p>
             </div>
           </div>
